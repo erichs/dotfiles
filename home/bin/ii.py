d0f914244c09dab5100caca5ce72b7f176de6872
Erich Smith <heartquest@gmail.com>
2013-04-03 15:21:38 -0400

    revise ii.py to accept argument

--- home/bin/ii.py
+++ home/bin/ii.py
@@ -25,27 +25,13 @@ nargs = len(sys.argv)
 
 random.seed()
 
-obf = open("~/bin/obsessions.txt", "r")
+obf = open(sys.argv[1], "r")
 obsessions = obf.readlines()
 maxobs = len(obsessions)
 
-if (nargs > 3):
-    print "python ii.py AAA BBB Googles both terms"
-    print "python ii.py AAA Googles one term"
-    print "python ii.py Googles two random terms"
-    sys.exit(1)
 
-if (nargs == 3):
-    arg1 = sys.argv[1]
-    arg2 = sys.argv[2]
-
-if (nargs == 2):
-    arg1 = sys.argv[1]
-    arg2 = random_obsession()
-
-if (nargs == 1):
-    arg1 = random_obsession()
-    arg2 = random_obsession()
+arg1 = random_obsession()
+arg2 = random_obsession()
 
 google = "http://www.google.com/search?num=100&q="
 search_url = google + munge(arg1) + "+" + munge(arg2)
