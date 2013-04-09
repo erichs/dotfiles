920eabde458498d0b97b78f04a989a0991b8408c
Erich Smith <heartquest@gmail.com>
2013-04-09 09:50:43 -0400

    added gg alias

--- home/.profile.d/08-git.sh
+++ home/.profile.d/08-git.sh
@@ -1,4 +1,5 @@
 alias gl='git l -10'
+alias gg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative --stat"
 alias gc='git commit'
 alias gs='git status -sb'
 alias ga='git add'
